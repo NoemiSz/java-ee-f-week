244d59770a06aa282614994fd00bb0474af88d78
Peter Blasko <blasko.peter.hu@gmail.com>
2019-03-19 11:24:38 +0100

    add mainSite.html and we have a logout button

--- frontend-service/src/main/java/com/codecool/frontendservice/controller/HomeController.java
+++ frontend-service/src/main/java/com/codecool/frontendservice/controller/HomeController.java
@@ -1,4 +1,19 @@
 package com.codecool.frontendservice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String mainSite(){
+        return "mainSite";
+    }
+
 }
