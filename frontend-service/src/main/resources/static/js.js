bee9ad75f7baf2e878adea3f78485b7548d7d024
Peter Blasko <blasko.peter.hu@gmail.com>
2019-03-21 17:02:29 +0100

    add new desing to index.html

--- frontend-service/src/main/resources/static/js.js
+++ frontend-service/src/main/resources/static/js.js
@@ -1,10 +1,12 @@
 function question(){
     let btn = document.getElementById("basic-question");
+    let helper = document.getElementById("helper-button");
     fetch("/question")
         .then(res => {
             return res.text();
         }).then(data =>
         btn.innerHTML = data).then(data => fetch("http://localhost:8084/say/" + data))
+        .then(helper.classList.remove("btn-hidden"))
 }
 
 function answer(){
