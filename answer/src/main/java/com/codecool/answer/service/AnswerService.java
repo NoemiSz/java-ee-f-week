7ca49366b58791b43127f599e5e86880a61d9004
NoemiSz <szokenoemijob@gmail.com>
2019-05-13 13:55:52 +0200

    answer API changed to https://api.chucknorris.io/jokes/random

--- answer/src/main/java/com/codecool/answer/service/AnswerService.java
+++ answer/src/main/java/com/codecool/answer/service/AnswerService.java
@@ -21,23 +21,12 @@ public class AnswerService {
 
 
     public String getAnswer(){
-        int high = 17;
-        int low = 1;
-        Random random = new Random();
-        int num = random.nextInt(high-low) + low;
-        List<String> ends = new ArrayList<>(Arrays.asList("/awesome/us","/cool/us","/everyone/:from",
-                "/everything/us","/flying/us","/give/us","/immensity/us", "/life/us",
-                "/looking/us","/mornin/us", "/no/us","/particular/question/us","/ratsarse/us",
-                "/sake/us","/shit/us","/what/us","/zero/us"));
-
-        String answer= "";
+        String chuckFact= "";
         try {
-            URL url = new URL("https://foaas.com"+ends.get(num));
+            URL url = new URL("https://api.chucknorris.io/jokes/random");
             HttpURLConnection urlConn =(HttpURLConnection)url.openConnection();
             urlConn.setRequestProperty("User-Agent",
                     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-            urlConn.setRequestProperty("Content-Type", "application/json");
-            urlConn.setRequestProperty("Accept", "application/json");
             urlConn.connect();
 
             InputStream is = urlConn.getInputStream();
@@ -45,9 +34,10 @@ public class AnswerService {
             BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             String str = "";
             while (null != (str = br.readLine())) {
+                System.out.println(str);
                 JSONObject jsonObject = new JSONObject(str);
-                answer = (String) jsonObject.get("message");
-                return answer;
+                chuckFact = (String) jsonObject.get("value");
+                return chuckFact;
             }
 
             br.close();
@@ -55,10 +45,8 @@ public class AnswerService {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
-        return answer;
+        return chuckFact;
     }
 }
 
